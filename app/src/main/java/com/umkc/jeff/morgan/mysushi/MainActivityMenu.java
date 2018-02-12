10c8fd79b74fef022e24b2630c478013515872ca
Jeff Morgan <quicksh0t12@gmail.com>
2018-02-11 22:17:18 -0600

    inital creation

---
+++ app/src/main/java/com/umkc/jeff/morgan/mysushi/MainActivityMenu.java
@@ -0,0 +1,13 @@
+package com.umkc.jeff.morgan.mysushi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
