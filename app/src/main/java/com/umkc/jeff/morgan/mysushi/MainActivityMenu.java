83e2275bcaede5869a3ab6915fddde0076c00b66
Jeff Morgan <quicksh0t12@gmail.com>
2018-02-15 22:57:58 -0600

    Functional buttons and taking to new activity.

--- app/src/main/java/com/umkc/jeff/morgan/mysushi/MainActivityMenu.java
+++ app/src/main/java/com/umkc/jeff/morgan/mysushi/MainActivityMenu.java
@@ -1,13 +1,62 @@
 package com.umkc.jeff.morgan.mysushi;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivityMenu extends AppCompatActivity {
+public class MainActivityMenu extends Activity {
+    Button sushiButton;
+    Button nigiriButton;
+    Button shashimiButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        // Locate the button in activity_main.xml
+        sushiButton = (Button) findViewById(R.id.sushi_button);
+
+        // Capture button clicks
+        sushiButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivityMenu.this,
+                        SushiActivityMenu.class);
+                startActivity(myIntent);
+            }
+        });
+
+        // Locate the button in activity_main.xml
+        nigiriButton = (Button) findViewById(R.id.nigiri_button);
+
+        // Capture button clicks
+        nigiriButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivityMenu.this,
+                        NigiriActivityMenu.class);
+                startActivity(myIntent);
+            }
+        });
+
+        // Locate the button in activity_main.xml
+        shashimiButton = (Button) findViewById(R.id.shashimi_button);
+
+        // Capture button clicks
+        shashimiButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivityMenu.this,
+                        ShashimiActivityMenu.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
