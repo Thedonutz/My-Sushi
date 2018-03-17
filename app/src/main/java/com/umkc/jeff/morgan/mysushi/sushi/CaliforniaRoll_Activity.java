aaaec14c0d003ac21036b172113bb754eea9dbbd
Jeff Morgan <quicksh0t12@gmail.com>
2018-03-17 11:12:29 -0500

    investigated POC for adding image upload and feasability

--- app/src/main/java/com/umkc/jeff/morgan/mysushi/sushi/CaliforniaRoll_Activity.java
+++ app/src/main/java/com/umkc/jeff/morgan/mysushi/sushi/CaliforniaRoll_Activity.java
@@ -1,7 +1,10 @@
 package com.umkc.jeff.morgan.mysushi.sushi;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.umkc.jeff.morgan.mysushi.R;
 
@@ -11,5 +14,29 @@ public class CaliforniaRoll_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_california_roll_);
+
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"),REQUEST_CODE);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            switch (requestCode) {
+
+                case REQUEST_CODE:
+                    if (resultCode == Activity.RESULT_OK) {
+                        //data gives you the image uri. Try to convert that to bitmap
+                        break;
+                    } else if (resultCode == Activity.RESULT_CANCELED) {
+                        Log.e(TAG, "Selecting picture cancelled");
+                    }
+                    break;
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Exception in onActivityResult : " + e.getMessage());
+        }
     }
 }
