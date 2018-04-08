d79c1c18c6f72bc6221e1ddc5f79414f857906be
Jeff Morgan <quicksh0t12@gmail.com>
2018-04-08 17:04:16 -0500

    added in placement holders and icons for all materials

---
+++ app/src/main/java/com/umkc/jeff/morgan/mysushi/sashimi/Akami_Sashimi.java
@@ -0,0 +1,15 @@
+package com.umkc.jeff.morgan.mysushi.sashimi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.umkc.jeff.morgan.mysushi.R;
+
+public class Akami_Sashimi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_akami__sashimi);
+    }
+}
