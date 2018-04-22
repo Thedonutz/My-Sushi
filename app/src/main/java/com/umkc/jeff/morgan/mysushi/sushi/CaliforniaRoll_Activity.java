a12440efb324882e76af0379561948f423955e3c
Jeff Morgan <quicksh0t12@gmail.com>
2018-04-22 00:18:03 -0500

    updates after rebuild

--- app/src/main/java/com/umkc/jeff/morgan/mysushi/sushi/CaliforniaRoll_Activity.java
+++ app/src/main/java/com/umkc/jeff/morgan/mysushi/sushi/CaliforniaRoll_Activity.java
@@ -11,7 +11,6 @@ public class CaliforniaRoll_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_california_roll);
-
     }
 
 }
